2f2e270540a1f14d2f5069188737bce09d1a3cec
tanmay <tanmay.shende@jarvis.consulting>
2021-05-14 00:35:15 +0530

    Fixed pagination issue.

--- indian_banks/views.py
+++ indian_banks/views.py
@@ -9,32 +9,23 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-class AutoCompleteView(ListAPIView, LimitOffsetPagination):
-    """Autocomplete API view for getting possible matches on the basis of branch name """
-    serializer_class = BranchSerialzier
-    renderer_classes = [JSONRenderer]
-
-    def get_queryset(self):
-        branch = self.request.query_params.get('q')
-        if branch:
-            queryset = Branches.objects.filter(branch__contains=branch).order_by('ifsc')
-            return queryset
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = BranchSerialzier(queryset, many=True)
-        if not queryset:
-            return Response({'message': 'Branch not found'}, status=status.HTTP_404_NOT_FOUND)
-        else:
-            return Response(data=serializer.data)
-
-
 class LargeResultsSetPagination(PageNumberPagination):
     """Pagination Controls"""
     page_size = 100
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
+
+class AutoCompleteView(ListAPIView, LimitOffsetPagination):
+    """Autocomplete API view for getting possible matches on the basis of branch name """
+    serializer_class = BranchSerialzier
+    renderer_classes = [JSONRenderer]
+
+    def get_queryset(self):
+        branch = self.request.query_params.get('q',None)
+        if branch:
+            queryset = Branches.objects.filter(branch__contains=branch).order_by('ifsc')
+            return queryset
 
 class SearchView(ListAPIView, LimitOffsetPagination):
     """Search API view for getting the possible matches across all columns and rows"""
